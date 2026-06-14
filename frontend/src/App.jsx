f7c8a1308b87eac35a7c4e1f990dbe5e6010f5ff
Eduardo Lezama <elezama625@gmail.com>
2026-06-14 13:08:02 -0600

    feat: configuracion principal, testing y CSS para el layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+// frontend/src/App.jsx
+import React from 'react';
+import TicketsPage from './modules/tickets/pages/TicketsPage';
+
+function App() {
+  return (
+    <TicketsPage />
+  );
+}
+
+export default App;
