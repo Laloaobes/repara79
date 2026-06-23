f750739b475286b1fa984a94415278c232d07456
Breyan21 <bryan.sebas.matias2001@gmail.com>
2026-06-23 05:25:34 -0600

    feat: conectar frontend con backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middlewareMode: false,
+    hmr: {
+      host: 'localhost',
+      port: 3000,
+      protocol: 'ws',
+    },
+  },
+  preview: {
+    port: 3000,
+  },
 })
